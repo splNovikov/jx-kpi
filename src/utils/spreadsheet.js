4717628669ab794c6ca127bdad366abe1b366b0e
pnovikov <novikovpa@yandex.ru>
2025-04-29 10:57:13 +0300

    fixes ss is not defined

--- src/utils/spreadsheet.js
+++ src/utils/spreadsheet.js
@@ -1,6 +1,6 @@
 function getSheetData(sheetName) {
-  const ss = SpreadsheetApp.getActiveSpreadsheet();
-  const sheet = ss.getSheetByName(sheetName);
+  const sSheet = SpreadsheetApp.getActiveSpreadsheet();
+  const sheet = sSheet.getSheetByName(sheetName);
   const data = sheet.getDataRange().getValues();
 
   return {
@@ -10,28 +10,28 @@ function getSheetData(sheetName) {
 }
 
 function updateCellValue(sheetName, rowIndex, columnIndex, value) {
-  const ss = SpreadsheetApp.getActiveSpreadsheet();
-  const sheet = ss.getSheetByName(sheetName);
+  const sSheet = SpreadsheetApp.getActiveSpreadsheet();
+  const sheet = sSheet.getSheetByName(sheetName);
   sheet.getRange(rowIndex + 2, columnIndex + 1).setValue(value);
 }
 
 function clearTargetSheet(targetSheetName) {
-  const ss = SpreadsheetApp.getActiveSpreadsheet();
-  const targetSheet = ss.getSheetByName(targetSheetName);
+  const sSheet = SpreadsheetApp.getActiveSpreadsheet();
+  const targetSheet = sSheet.getSheetByName(targetSheetName);
   targetSheet.clear();
 }
 
 function copyDataToTargetSheet(targetSheetName, sourceSheetName) {
-
-  const targetSheet = ss.getSheetByName(targetSheetName);
-  const sourceSheet = ss.getSheetByName(sourceSheetName);
+  const sSheet = SpreadsheetApp.getActiveSpreadsheet();
+  const targetSheet = sSheet.getSheetByName(targetSheetName);
+  const sourceSheet = sSheet.getSheetByName(sourceSheetName);
   const sourceData = sourceSheet.getDataRange().getValues();
   targetSheet.getRange(1, 1, sourceData.length, sourceData[0].length).setValues(sourceData);
 }
 
 function addLastColumnTitle(targetSheetName, title) {
-  const ss = SpreadsheetApp.getActiveSpreadsheet();
-  const targetSheet = ss.getSheetByName(targetSheetName);
+  const sSheet = SpreadsheetApp.getActiveSpreadsheet();
+  const targetSheet = sSheet.getSheetByName(targetSheetName);
   const lastColumn = targetSheet.getLastColumn();
   targetSheet.getRange(1, lastColumn + 1).setValue(title);
 }
